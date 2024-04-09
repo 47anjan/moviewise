67b5c3f3e99e29e6701a316b3e2215b7b60e39bb
anjankarmakar10 <anjankarmakar15@gmail.com>
2024-04-09 14:16:22 +0600

    Build search drawer

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-[200vh]">
       <Hero />
     </main>
   );
--- components/search.tsx
+++ components/search.tsx
@@ -2,10 +2,45 @@
 import React from "react";
 import { HoverBorderGradient } from "./hover-border-gradient";
 import { Search as IconSearch } from "lucide-react";
+import { motion } from "framer-motion";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 export function Search() {
   return (
-    <div className=" flex justify-center text-center">
+    <Drawer>
+      <DrawerTrigger>
+        <SearchButton />
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          Submit
+          <DrawerClose>Cancel</DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+const SearchButton = () => {
+  return (
+    <motion.div
+      whileTap={{ scale: 0.8 }}
+      className=" flex justify-center text-center"
+    >
       <HoverBorderGradient
         containerClassName="rounded-full"
         as="button"
@@ -14,6 +49,6 @@ export function Search() {
         <IconSearch size={32} />
         <span>Wanna Search</span>
       </HoverBorderGradient>
-    </div>
+    </motion.div>
   );
-}
+};
